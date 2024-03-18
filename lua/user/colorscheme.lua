729ab0681b0e4e4471cfc3a62b18123915c5d070
Nikola Milenkovic <nikola.milenkovic@symphony.is>
2024-03-18 22:53:18 +0000

    update lunar

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,8 @@
-local M = {
-  "LunarVim/darkplus.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-}
+-- local M = {
+--   "LunarVim/darkplus.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+-- }
 
 -- local M = {
 --   "rose-pine/neovim",
@@ -16,14 +16,14 @@ local M = {
 --     lazy = false,
 --   }
 
--- local M = {
---   "projekt0n/github-nvim-theme",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
--- }
+local M = {
+  "LunarVim/lunar.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "lunar"
 end
 
 return M
